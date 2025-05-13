535cf0c81ca4d88634a4f0e204f52db2a80fa0da
Caterina1231 <caterina.neymar@gmail.com>
2025-05-13 09:55:34 +0530

    Update Hashing.c

--- Hashing.c
+++ Hashing.c
@@ -1,8 +1,7 @@
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
 
 #define MAX 10
 
@@ -10,74 +9,131 @@ int HashTable[MAX];
 
 void init()
 {
-    for(int i=0;i<MAX;i++)
+    for(int i = 0; i < MAX; i++)
     {
-        HashTable[i]=-1;
+        HashTable[i] = -1;
     }
 }
 
 int HashFunction(int key)
 {
-    return key%MAX;
+    return key % MAX;
+}
+
+bool isPrime(int num)
+{
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0)
+            return false;
+    return true;
+}
+
+int findPrimeBelowMax()
+{
+    for (int i = MAX - 1; i >= 2; i--)
+        if (isPrime(i))
+            return i;
+    return 2;
 }
 
 int secondHash(int key)
 {
-    return 7-(key%7);
+    int prime = findPrimeBelowMax();
+    return prime - (key % prime);
+}
+
+void InsertUsingNormalHash(int key)
+{
+    int index = HashFunction(key);
+    if (HashTable[index] == -1)
+    {
+        HashTable[index] = key;
+    }
+    else
+    {
+        printf("\nCollision occurred at index %d. Normal Hashing doesn't resolve it.\n", index);
+    }
 }
 
 void InsertUsingLinearProbing(int key)
 {
-    int i=0;
-    int index=HashFunction(key);
-    while(HashTable[(index+i)%MAX]!=-1 && i<MAX )
+    int i = 0;
+    int index = HashFunction(key);
+    while (HashTable[(index + i) % MAX] != -1 && i < MAX)
     {
         i++;
     }
-    if(i<MAX)
+    if (i < MAX)
     {
-      HashTable[(index+i)%MAX]=key;
+        HashTable[(index + i) % MAX] = key;
     }
     else
     {
-        printf("\n Hash Table is Full.");
+        printf("\nHash Table is Full (Linear Probing).\n");
     }
 }
 
 void InsertUsingQuadraticProbing(int key)
 {
-    int i=0;
-    int index=HashFunction(key);
-    while(HashTable[(index+(i*i))%MAX]!=-1 && i<MAX )
+    int i = 0;
+    int index = HashFunction(key);
+    while (HashTable[(index + (i * i)) % MAX] != -1 && i < MAX)
     {
         i++;
     }
-    if(i<MAX)
+    if (i < MAX)
     {
-      HashTable[(index+(i*i))%MAX]=key;
+        HashTable[(index + (i * i)) % MAX] = key;
     }
     else
     {
-        printf("\n Hash Table is Full.");
+        printf("\nHash Table is Full (Quadratic Probing).\n");
     }
 }
 
-void InsertusingDoubleProbing(int key)
+void InsertUsingDoubleProbing(int key)
 {
-    int i=0;
-    int index1=HashFunction(key);
-    int index2=secondHash(key);
+    int i = 0;
+    int index1 = HashFunction(key);
+    int index2 = secondHash(key);
 
-    while(HashTable[index1+(index2*i)%MAX]!=-1 && i<MAX )
+    while (HashTable[(index1 + (index2 * i)) % MAX] != -1 && i < MAX)
     {
         i++;
     }
-    if(i<MAX)
+    if (i < MAX)
     {
-      HashTable[index1+(index2*i)%MAX]=key;
+        HashTable[(index1 + (index2 * i)) % MAX] = key;
     }
     else
     {
-        printf("\n Hash Table is Full.");
+        printf("\nHash Table is Full (Double Hashing).\n");
     }
+}
+
+void display()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        if (HashTable[i] != -1)
+            printf("%d -> %d\n", i, HashTable[i]);
+        else
+            printf("%d -> \n", i);
+    }
+}
+
+int main()
+{
+    init();
+
+    InsertUsingNormalHash(15);
+    InsertUsingNormalHash(25);
+
+    InsertUsingLinearProbing(35);
+    InsertUsingQuadraticProbing(45);
+    InsertUsingDoubleProbing(55);
+
+    display();
+    return 0;
 }
